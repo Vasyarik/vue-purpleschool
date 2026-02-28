f4c46c1437a4770f0bf8cd7c0d4b0c3828c039ce
Yaroslav Vasilev <yaroslav.vasilyev@iss.ru>
2026-02-28 15:30:55 +0300

    Add Card to App for show example

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
 import Header from "@/components/Header.vue";
+import Card from "@/components/Card.vue";
 
 const currentDate = new Date();
 </script>
 
 <template>
   <Header />
+
+  <Card
+      number="1"
+      word="test"
+      translate="Тест"
+  />
 </template>
 
 <style scoped>
