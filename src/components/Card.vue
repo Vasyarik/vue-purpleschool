3922d4cba0915ee79095dbe0a01047375495244d
Yaroslav Vasilev <yaroslav.vasilyev@iss.ru>
2026-03-02 10:37:32 +0300

    Add status elements to card back and set events handlers for emitted events

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref } from "vue";
+import FailIcon from "@/components/icons/FailIcon.vue";
+import SuccessIcon from "@/components/icons/SuccessIcon.vue";
 
-defineEmits(['flip', 'status-change'])
+const emits = defineEmits(['flip', 'status-change'])
 
 const props = defineProps({
   number:    {
@@ -20,25 +22,63 @@ const props = defineProps({
 
 const { number } = props;
 
+const chosenStatus = ref(null);
+
+const flipped = ref(false);
+
 function formatNumber() {
   return number < 10 ? '0' + number : number;
+}
+
+function flipHandler() {
+  flipped.value = !flipped.value;
+
+  emits('flip');
+}
+
+function statusChangeHandler(status) {
+  chosenStatus.value = status;
+
+  console.log(status)
+
+  emits('status-change', status);
 }
 </script>
 
 <template>
-  <div class="card">
+  <div :class="{card: true, flipped}">
     <div class="card-inner">
       <div class="card-front">
         <div class="card-content">
           <div class="card-number">{{ formatNumber() }}</div>
           <div class="card-word">{{ word }}</div>
-          <div class="card-bottom">ПЕРЕВЕНУТЬ</div>
+          <div class="card-bottom" @click="flipHandler">ПЕРЕВЕНУТЬ</div>
         </div>
       </div>
       <div class="card-back">
         <div class="card-content">
           <div class="card-number">{{ formatNumber() }}</div>
+          <div class="card-status-icon" v-if="chosenStatus">
+            <SuccessIcon
+                width="48px"
+                height="48px"
+                v-if="chosenStatus === 'success'"
+                @click=""
+            />
+            <FailIcon
+                width="48px"
+                height="48px"
+                v-if="chosenStatus === 'fail'"
+            />
+          </div>
           <div class="card-word">{{ translate }}</div>
+          <div class="card-bottom">
+            <div class="choose-status" v-if="!chosenStatus">
+              <FailIcon @click="statusChangeHandler('fail')" />
+              <SuccessIcon @click="statusChangeHandler('success')"/>
+            </div>
+            <span v-else>ЗАВЕРШЕНО</span>
+          </div>
         </div>
       </div>
     </div>
@@ -63,7 +103,7 @@ function formatNumber() {
   transform-style: preserve-3d;
 }
 
-.card:hover .card-inner {
+.card.flipped .card-inner {
   transform: rotateY(180deg);
 }
 
@@ -80,8 +120,6 @@ function formatNumber() {
   position: absolute;
   -webkit-backface-visibility: hidden;
   backface-visibility: hidden;
-
-  cursor: pointer;
 }
 
 .card:hover .card-front,
@@ -119,6 +157,13 @@ function formatNumber() {
   text-align: center;
 }
 
+.card-status-icon {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
 .card-word {
   font-weight: normal;
   font-size: 18px;
@@ -141,5 +186,19 @@ function formatNumber() {
   text-transform: uppercase;
 
   color: var(--color-font-accent);
+}
+
+.card-front .card-bottom {
+  cursor: pointer;
+}
+
+.choose-status {
+  display: flex;
+  align-items: center;
+  gap: 32px;
+}
+
+.choose-status svg {
+  cursor: pointer;
 }
 </style>
